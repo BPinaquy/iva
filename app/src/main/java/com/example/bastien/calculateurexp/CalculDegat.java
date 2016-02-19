0ac682cc158b1898833231429bd935ae8c8b3fb9
Bastien Pinaquy <bastien.pinaquy@intello.com>
2016-02-19 19:11:23 +0100

    premier

---
+++ app/src/main/java/com/example/bastien/calculateurexp/CalculDegat.java
@@ -0,0 +1,116 @@
+package com.example.bastien.calculateurexp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class CalculDegat extends ActionBarActivity {
+    private EditText chance, attaque, bonus, de, carac, technique;
+    private Button calcul;
+    private TextView degat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcul_degat);
+        RelativeLayout relative = (RelativeLayout) findViewById(R.id.relative);
+        relative.setOnTouchListener(new com.example.bastien.calculateurexp.OnSwipeListener() {
+            @Override
+            public void onSwipeRight() {
+                Intent intent = new Intent(CalculDegat.this, CalculExp.class);
+                startActivity(intent);
+
+            }
+        });
+        degat = (TextView) findViewById(R.id.degat);
+        chance = (EditText) findViewById(R.id.editTextChance);
+        attaque = (EditText) findViewById(R.id.editTextAttaque);
+        bonus = (EditText) findViewById(R.id.editTextBonus);
+        de = (EditText) findViewById(R.id.editTextDe);
+        technique = (EditText) findViewById(R.id.editTextTechnique);
+        carac = (EditText) findViewById(R.id.editTextCarac);
+
+        calcul = (Button) findViewById(R.id.button2);
+        calcul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int degatTotal = 0, chanceVal = 0, attaqueVal = 0, deVal = 0, bonusVal = 0, caracVal = 0, techniqueVal = 0;
+                if (!chance.getText().toString().equals("")) {
+                    chanceVal = Integer.valueOf(chance.getText().toString());
+                }
+
+                if (!attaque.getText().toString().equals("")) {
+                    attaqueVal = Integer.valueOf(attaque.getText().toString());
+                }
+
+                if (!de.getText().toString().equals("")) {
+                    deVal = Integer.valueOf(de.getText().toString());
+                }
+
+                if (!bonus.getText().toString().equals("")) {
+                    bonusVal = Integer.valueOf(bonus.getText().toString());
+                }
+
+                if (!carac.getText().toString().equals("")) {
+                    caracVal = Integer.valueOf(carac.getText().toString());
+                }
+
+                if (!technique.getText().toString().equals("")) {
+                    techniqueVal = Integer.valueOf(technique.getText().toString());
+                }
+
+
+                if (deVal == 1 | deVal == 2) {
+                    degat.setText("0");
+                } else {
+                    if (deVal == 20 || deVal == 19) {
+                        degatTotal += deVal * 2;
+                    } else if ((deVal == 18 || deVal == 17) && chanceVal == 2) {
+                        degatTotal += deVal * 2;
+                    } else {
+                        degatTotal += deVal;
+                    }
+                    degatTotal += attaqueVal + bonusVal + (techniqueVal * caracVal /100);
+                    degat.setText(String.valueOf(degatTotal));
+                }
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calcul_degat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/bastien/calculateurexp/CalculExp.java
@@ -0,0 +1,80 @@
+package com.example.bastien.calculateurexp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class CalculExp extends ActionBarActivity {
+    private Button button;
+    private TextView resultat;
+    private EditText depart, arrive;
+    private int valDepart, valArrive;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculexp);
+        button = (Button)findViewById(R.id.button);
+        resultat=(TextView)findViewById(R.id.resultat);
+        depart=(EditText)findViewById(R.id.editText);
+        arrive=(EditText)findViewById(R.id.editText2);
+        final int experience[]={0,50,50,50,50,100,100,100,100,100,150,200,250,300,350,400,450,500,550,600,700,800,900,1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000};
+        experience[0]=0;
+        RelativeLayout relative = (RelativeLayout)findViewById(R.id.relative);
+        relative.setOnTouchListener(new OnSwipeListener(){
+            @Override
+            public void onSwipeLeft() {
+                Intent intent = new Intent(CalculExp.this,CalculDegat.class);
+                startActivity(intent);
+
+            }});
+
+
+            button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int somme=0;
+                valDepart = Integer.parseInt(depart.getText().toString());
+                valArrive = Integer.parseInt(arrive.getText().toString());
+                for(int i =valDepart;i<valArrive;i++){
+                    somme+=experience[i];
+                }
+                resultat.setText(String.valueOf(somme));
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
